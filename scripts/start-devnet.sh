0b8a97bcc4d6c82a5a80167a794d0a8f5ab75e26
Simon Letort <simon.letort@digitalasset.com>
2025-03-30 17:42:43 -0400

    init

---
+++ scripts/start-devnet.sh
@@ -0,0 +1,3 @@
+# ./start.sh -s "<SPONSOR_SV_URL>" -o "<ONBOARDING_SECRET>" -p "prometheus-validator-1" -m "<MIGRATION_ID>" -w
+
+./start.sh -s "https://scan.sv-1.dev.global.canton.network.digitalasset.com" -o "<ONBOARDING_SECRET>" -p "simon-test-1" -m "1" -w
