46857e4f3bbd60845b7c51ecc92954d775e9e521
Simon Letort <simon.letort@digitalasset.com>
2025-03-31 07:51:36 -0400

    onboarding secret script (#2)
    * init
    
    * script start devnet
    
    * Update start-devnet.sh
    
    * Update README.md
    
    * Update start-devnet.sh
    
    * onboarding secret script

--- scripts/start-devnet.sh
+++ scripts/start-devnet.sh
@@ -4,20 +4,22 @@ echo "###################################"
 echo "Deploying Canton validator"
 echo "###################################"
 
+# set here your party hint, format company-role-increment, i.e. simon-validator-1
+party_hint="simon-validator-1"
+echo "party_hint="${party_hint}
+
 export IMAGE_TAG=0.3.18
 echo "IMAGE_TAG="${IMAGE_TAG}
 
 SPONSOR_SV_URL="https://sv.sv-1.dev.global.canton.network.sync.global"
+#SPONSOR_SV_URL="https://sv.sv-2.dev.global.canton.network.digitalasset.com"
 echo "SPONSOR_SV_URL="${SPONSOR_SV_URL}
-
-party_hint="simon-validator-1"
-echo "party_hint="${party_hint}
 
 MIGRATION_ID=1
 echo "MIGRATION_ID="${MIGRATION_ID}
 
 echo "Requesting onboarding secret from SPONSOR_SV "${SPONSOR_SV_URL}
-ONBOARDING_SECRET=$(curl -X POST https://sv.sv-1.dev.global.canton.network.sync.global/api/sv/v0/devnet/onboard/validator/prepare)
+ONBOARDING_SECRET=$(curl -X POST ${SPONSOR_SV_URL}/api/sv/v0/devnet/onboard/validator/prepare)
 
 echo "Starting the validator node"
 ./splice-node/docker-compose/validator/start.sh -s "${SPONSOR_SV_URL}" -o "${ONBOARDING_SECRET}" -p "${party_hint}" -m "${MIGRATION_ID}" -w
