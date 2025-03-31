38106d0c4e354d6ffa138b065f7beb47bb6a3917
Simon Letort <simon.letort@digitalasset.com>
2025-03-31 00:11:37 -0400

    Update start-devnet.sh

--- scripts/start-devnet.sh
+++ scripts/start-devnet.sh
@@ -10,6 +10,7 @@ echo "IMAGE_TAG="${IMAGE_TAG}
 SPONSOR_SV_URL="https://sv.sv-1.dev.global.canton.network.sync.global"
 echo "SPONSOR_SV_URL="${SPONSOR_SV_URL}
 
+# set here your party hint, format company-role-increment, i.e. simon-validator-1
 party_hint="simon-validator-1"
 echo "party_hint="${party_hint}
 
@@ -17,7 +18,7 @@ MIGRATION_ID=1
 echo "MIGRATION_ID="${MIGRATION_ID}
 
 echo "Requesting onboarding secret from SPONSOR_SV "${SPONSOR_SV_URL}
-ONBOARDING_SECRET=$(curl -X POST https://sv.sv-1.dev.global.canton.network.sync.global/api/sv/v0/devnet/onboard/validator/prepare)
+ONBOARDING_SECRET=$(curl -X POST ${SPONSOR_SV_URL}/api/sv/v0/devnet/onboard/validator/prepare)
 
 echo "Starting the validator node"
 ./splice-node/docker-compose/validator/start.sh -s "${SPONSOR_SV_URL}" -o "${ONBOARDING_SECRET}" -p "${party_hint}" -m "${MIGRATION_ID}" -w
