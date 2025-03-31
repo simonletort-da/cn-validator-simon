935315f414f99d3a5f245b177d8d0a46b90a4a47
Simon Letort <simon.letort@digitalasset.com>
2025-03-30 22:14:17 -0400

    script start devnet

--- scripts/start-devnet.sh
+++ scripts/start-devnet.sh
@@ -1,3 +1,21 @@
-# ./start.sh -s "<SPONSOR_SV_URL>" -o "<ONBOARDING_SECRET>" -p "prometheus-validator-1" -m "<MIGRATION_ID>" -w
+# https://dev.network.canton.global/validator_operator/validator_compose.html#deployment
 
-./start.sh -s "https://scan.sv-1.dev.global.canton.network.digitalasset.com" -o "<ONBOARDING_SECRET>" -p "simon-test-1" -m "1" -w
+echo "Deploying Canton validator"
+
+export IMAGE_TAG=0.3.18
+echo "IMAGE_TAG="${IMAGE_TAG}
+
+SPONSOR_SV_URL="https://sv.sv-1.dev.global.canton.network.sync.global"
+echo "SPONSOR_SV_URL="${SPONSOR_SV_URL}
+
+party_hint="simon-validator-1"
+echo "party_hint="${party_hint}
+
+MIGRATION_ID=1
+echo "MIGRATION_ID="${MIGRATION_ID}
+
+echo "Requesting onboarding secret from SPONSOR_SV "${SPONSOR_SV_URL}
+ONBOARDING_SECRET=$(curl -X POST https://sv.sv-1.dev.global.canton.network.sync.global/api/sv/v0/devnet/onboard/validator/prepare)
+
+echo "Starting the validator node"
+./splice-node/docker-compose/validator/start.sh -s "${SPONSOR_SV_URL}" -o "${ONBOARDING_SECRET}" -p "${party_hint}" -m "${MIGRATION_ID}" -w
